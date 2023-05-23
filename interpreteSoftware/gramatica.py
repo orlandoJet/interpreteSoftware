2e13bfde90ebc0b987f950a66406edfceffc9f7a
Eliasib <eliasibruzguevara@gmail.com>
2023-05-23 00:00:27 -0500

    formulas aritmeticas y comparaciones

--- interpreteSoftware/gramatica.py
+++ interpreteSoftware/gramatica.py
@@ -1,18 +1,18 @@
 import ply.lex as lex
 
 palabrasReservadas = {
-    "ver" : "print", #print
-    "obt": "input",#input
-    "es": "if", #if
-    "deloc": "else", #else
-    "yy": "and", #and
-    "oo": "or", #or
-    "negar": "not", #not
-    "ciclom": "while", #whie
-    "ciclop": "for", #for
-    "func": "def", #def
-    "fin": "break", #break
-    "devol": "return" #return
+    "ver": "print",  # print
+    "obt": "input",  # input
+    "es": "if",  # if
+    "deloc": "else",  # else
+    "yy": "and",  # and
+    "oo": "or",  # or
+    "negar": "not",  # not
+    "ciclom": "while",  # while
+    "ciclop": "for",  # for
+    "func": "def",  # def
+    "fin": "break",  # break
+    "devol": "return"  # return
 }
 
 literales = (
@@ -29,7 +29,7 @@ tiposDeToken = (
 tokens = [
     'ID',
     'IMPRIMIR',
-    'LEER'
+    'LEER',
     'SI',
     'SINO',
     'Y',
@@ -39,7 +39,6 @@ tokens = [
     'FUNCION',
     'BREAK',
     'RETORNAR',
-
     'REVALUAR',
     'PARIZQ',
     'PARDER',
@@ -57,74 +56,86 @@ tokens = [
     'MENORIGUAL',
     'MAYORIGUAL',
     'IGUAL',
+    'ASIGNACION',
     'DISTINTO',
-    'IDENTIFICADOR'  # Nuevo token para identificadores
-]+ list(palabrasReservadas.values())
+    'IDENTIFICADOR'
+] + list(palabrasReservadas.values())
 
 
 # Tokens
-t_REVALUAR  = r'Evaluar'
-t_PARIZQ    = r'\('
-t_PARDER    = r'\)'
-t_CORIZQ    = r'\['
-t_CORDER    = r'\]'
-t_MAS       = r'\+'
-t_MENOS     = r'-'
-t_POR       = r'\*'
-t_DIVIDIDO  = r'/'
-t_PTCOMA    = r';'
-t_MENOR     = r'<'
-t_MAYOR     = r'>'
+t_REVALUAR = r'Evaluar'
+t_PARIZQ = r'\('
+t_PARDER = r'\)'
+t_CORIZQ = r'\['
+t_CORDER = r'\]'
+t_MAS = r'\+'
+t_MENOS = r'-'
+t_POR = r'\*'
+t_DIVIDIDO = r'/'
+t_PTCOMA = r';'
+t_MENOR = r'<'
+t_MAYOR = r'>'
 t_MENORIGUAL = r'<='
 t_MAYORIGUAL = r'>='
-t_IGUAL     = r'=='
-t_DISTINTO  = r'!='
-
+t_IGUAL = r'=='
+t_ASIGNACION = r'='
+t_DISTINTO = r'!='
 
 
 def t_IMPRIMIR(t):
     r'ver'
     return t
 
+
 def t_LEER(t):
     r'obt'
     return t
+
 
 def t_SI(t):
     r'es'
     return t
 
+
 def t_SINO(t):
     r'deloc'
     return t
+
 
 def t_Y(t):
     r'yy'
     return t
 
+
 def t_O(t):
     r'oo'
     return t
 
+
 def t_NEGAR(t):
-    r'neg'
+    r'negar'
     return t
+
 
 def t_MIESTRAS(t):
-    r'repetir'
+    r'ciclom'
     return t
 
+
 def t_FUNCION(t):
-    r'func'
+    r'ciclop'
     return t
+
 
 def t_BREAK(t):
     r'fin'
     return t
 
+
 def t_RETORNAR(t):
     r'devol'
     return t
+
 
 def t_DECIMAL(t):
     r'\d+\.\d+'
@@ -135,6 +146,7 @@ def t_DECIMAL(t):
         t.value = 0
     return t
 
+
 def t_ENTERO(t):
     r'\d+'
     try:
@@ -144,6 +156,7 @@ def t_ENTERO(t):
         t.value = 0
     return t
 
+
 # Caracteres ignorados
 t_ignore = " \t"
 
@@ -151,51 +164,60 @@ t_ignore = " \t"
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += t.value.count("\n")
-    
+
+
 def t_error(t):
     print("Caracter invalido '%s'" % t.value[0])
     t.lexer.skip(1)
-    
+
+
 # Construyendo el analizador léxico
-import ply.lex as lex
 lexer = lex.lex()
 
 
 # Asociación de operadores y precedencia
 precedence = (
-    ('left','MAS','MENOS'),
-    ('left','POR','DIVIDIDO'),
-    ('left','MENOR','MAYOR','MENORIGUAL','MAYORIGUAL','IGUAL','DISTINTO'),
-    ('right','UMENOS'),
-    )
+    ('left', 'MAS', 'MENOS'),
+    ('left', 'POR', 'DIVIDIDO'),
+    ('left', 'MENOR', 'MAYOR', 'MENORIGUAL', 'MAYORIGUAL', 'IGUAL', 'DISTINTO', 'ASIGNACION'),
+    ('right', 'UMENOS'),
+)
+
 
 def p_instrucciones_evaluar(t):
     'instruccion : REVALUAR CORIZQ expresion CORDER PTCOMA'
     print('El valor de la expresión es: ' + str(t[3]))
+
 
 def p_expresion_binaria(t):
     '''expresion : expresion MAS expresion
                   | expresion MENOS expresion
                   | expresion POR expresion
                   | expresion DIVIDIDO expresion'''
-    if t[2] == '+'  : t[0] = t[1] + t[3]
-    elif t[2] == '-': t[0] = t[1] - t[3]
-    elif t[2] == '*': t[0] = t[1] * t[3]
-    elif t[2] == '/': t[0] = t[1] / t[3]
+    if t[2] == '+':
+        t[0] = t[1] + t[3]
+    elif t[2] == '-':
+        t[0] = t[1] - t[3]
+    elif t[2] == '*':
+        t[0] = t[1] * t[3]
+    elif t[2] == '/':
+        t[0] = t[1] / t[3]
+
 
 def p_expresion_unitaria(t):
     'expresion : MENOS expresion %prec UMENOS'
     t[0] = -t[2]
 
+
 def p_expresion_agrupacion(t):
     'expresion : PARIZQ expresion PARDER'
     t[0] = t[2]
+
 
 def p_expresion_number(t):
     '''expresion    : ENTERO
                     | DECIMAL'''
     t[0] = t[1]
-
 
 
 def p_expresion_condicional(t):
@@ -204,24 +226,35 @@ def p_expresion_condicional(t):
                 | expresion MENORIGUAL expresion
                 | expresion MAYORIGUAL expresion
                 | expresion IGUAL expresion
-                | expresion DISTINTO expresion'''
+                | expresion DISTINTO expresion
+                | expresion ASIGNACION expresion'''
 
     t[0] = t[1] < t[3]
-    if t[2] == '<':     t[0] = t[1] < t[3]
-    elif t[2] == '>':   t[0] = t[1] > t[3]
-    elif t[2] == '<=':  t[0] = t[1] <= t[3]
-    elif t[2] == '>=':  t[0] = t[1] >= t[3]
-    elif t[2] == '==':  t[0] = t[1] == t[3]
-    elif t[2] == '!=':  t[0] = t[1] != t[3]
+    if t[2] == '<':
+        t[0] = t[1] < t[3]
+    elif t[2] == '>':
+        t[0] = t[1] > t[3]
+    elif t[2] == '<=':
+        t[0] = t[1] <= t[3]
+    elif t[2] == '>=':
+        t[0] = t[1] >= t[3]
+    elif t[2] == '==':
+        t[0] = t[1] == t[3]
+    elif t[2] == '!=':
+        t[0] = t[1] != t[3]
+    elif t[2] == '=':
+        t[0] = t[3]
 
 
 def p_error(t):
     print("Error sintáctico en '%s'" % t.value)
 
+
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
     t.type = palabrasReservadas.get(t.value, 'ID')  # Verificar palabras reservadas
     return t
+
 
 def p_instrucciones_lista(t):
     '''
@@ -230,6 +263,7 @@ def p_instrucciones_lista(t):
     '''
     pass  # Esta regla no hace nada por ahora
 
+
 def p_instruccion(t):
     '''
     instruccion : ID '=' expresion PTCOMA
@@ -237,11 +271,23 @@ def p_instruccion(t):
     t[0] = t[3]  # Almacenar el valor de la expresión en la variable asignada
     print('El valor de ' + t[1] + ' es: ' + str(t[0]))  # Imprimir el valor de la variable asignada
 
+    # Escribir el valor de x en un archivo .txt
+    if t[1] == 'x':
+        t[0] = t[3]
+        escribir_archivo('El valor de x es:', str(t[0]))
+
+
+def escribir_archivo(nombre_archivo, contenido):
+    with open(nombre_del_archivo, 'w') as archivo:
+        archivo.write(contenido)
+
+nombre_del_archivo = "c:/Users/user/Downloads/interpreteSoftware/interpreteSoftware/entrada.txt"  
+
 import ply.yacc as yacc
 parser = yacc.yacc()
 
 
-f = open("c:/Users/user/Downloads/interpreteSoftware/interpreteSoftware/entrada.txt", "r")
+f = open(nombre_del_archivo, "r")
 input = f.read()
 print(input)
 parser.parse(input)
